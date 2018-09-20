c4ad2b3b9bd6cc8c2b6d3dd8c59d60f6b11f2d6a
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-09-20 13:20:24 +0700

    v1.0

---
+++ yo.js
@@ -0,0 +1,2 @@
+ball2.xSpeed = ((ball2.radius -ball1.radius)*ball2.xSpeed+2*ball1.radius*ball1.xSpeed)/ballMaxDistance;
+ball2.ySpeed = ((ball2.radius -ball1.radius)*ball2.ySpeed+2*ball1.radius*ball1.ySpeed)/ballMaxDistance;
